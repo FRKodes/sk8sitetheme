ec730af6b0466067090a77cfd9a7edd31503c241
Antonio Calderón <tony@Antonios-MacBook-Pro.local>
2020-04-13 00:00:13 -0500

    Adding Ads to the post detail page

--- single.php
+++ single.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -22,10 +22,26 @@ get_header(); ?>
 
 		endwhile; // End of the loop.
 		?>
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-12 text-center" style="margin-top: 40px;">
+					<div class="banner-google-container-feed">
+						<ins class="adsbygoogle"
+						     style="display:block"
+						     data-ad-client="ca-pub-6490955290269477"
+						     data-ad-slot="1652665079"
+						     data-ad-format="auto"
+						     data-full-width-responsive="true"></ins>
+						<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+					</div>
+				</div>
+			</div>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+	
 <?php
 do_action( 'storefront_sidebar' );
 get_footer();
